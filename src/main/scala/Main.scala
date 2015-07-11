e61d090a875db8563236c72d71c1f833a620dcfd
Dorota Celińska <dorota.celinska@gmail.com>
2015-07-11 17:32:10 +0200

    improved logic, implmented toString()

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,26 +1,32 @@
 import scala.collection.mutable.ArrayBuffer
 import scala.util.Random
+import scala.util.Try
 
 class Board(width: Int, height: Int) {
   type Creature = Boolean
   private type BoardType = ArrayBuffer[ArrayBuffer[Creature]]
 
-  def newBoard(width: Int, height: Int): BoardType =
-    ArrayBuffer.fill[Creature](width, height){Random.nextBoolean()}
+  def newBoard(width: Int, height: Int)(func: => Creature): BoardType =
+    ArrayBuffer.fill[Creature](width, height){func}
 
-  var board = newBoard(width, height)
+  var board = newBoard(width, height){Random.nextBoolean()}
 
   def willLive(x: Int, y: Int): Boolean = {
     var livingNeighbours = 0
     for {
-      dx <- Range(-1,1)
-      dy <- Range(-1,1)
-      if dx != 0 || dy != 0
+      dx <- Range(-1, 1).inclusive
+      dy <- Range(-1, 1).inclusive
+      if (dx != 0 || dy != 0)
     } {
-        if (board(x + dx)(y+dy)==true) {
-          livingNeighbours+=1
+      try {
+        if (board(x + dx)(y + dy)==true) {
+          livingNeighbours += 1
         }
+      } catch {
+        case e: IndexOutOfBoundsException => ()
+      }
     }
+
     if ((board(x)(y)==true && (livingNeighbours==2 || livingNeighbours==3)) ||
         (board(x)(y)==false && livingNeighbours==3)) {
       return true
@@ -29,8 +35,8 @@ class Board(width: Int, height: Int) {
     }
   }
 
-  def nextGeneration(board: BoardType): BoardType = {
-    var nextBoard = newBoard(width, height)
+  private def computeGeneration: BoardType = {
+    var nextBoard = newBoard(width, height){false}
     for {
       x <- Range(0, width)
       y <- Range(0, height)
@@ -39,10 +45,34 @@ class Board(width: Int, height: Int) {
     }
     nextBoard
   }
+
+  def nextGeneration() =
+    board = computeGeneration
+
+  override def toString: String = {
+    val rows = for(y <- Range(0, height)) yield {
+      val row = for(x <- Range(0, width)) yield {
+        if (board(x)(y))
+          "\033[32mX\033[0m"
+        else
+          "\033[31mX\033[0m"
+      }
+      row.mkString
+    }
+    rows.mkString("\n")
+  }
 }
 
 object Main {
   def main(args: Array[String]) = {
-
+    val gol = new Board(
+      Try(args(0)).getOrElse("40").toInt,
+      Try(args(1)).getOrElse("40").toInt)
+    while (true) {
+      print("\n")
+      println(gol)
+      Thread.sleep(100)
+      gol.nextGeneration()
+    }
   }
 }
